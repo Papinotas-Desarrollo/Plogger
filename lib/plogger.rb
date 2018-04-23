a80e596c176922f9758340f2bc596197f4d16206
Daniel Merrill <danielmerrill6@gmail.com>
2018-04-23 13:00:40 -0300

    Added log severity

--- lib/plogger.rb
+++ lib/plogger.rb
@@ -23,30 +23,30 @@ module Plogger
   def exception(exception, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_exception(exception, category: category, user_id: user_id, account_id: account_id,
-                                        extra_info: extra_info)
+                                        extra_info: extra_info.merge({log_severity: 'exception'}))
   end
 
   def error(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_error(message, category: category, user_id: user_id, account_id: account_id,
-                                  extra_info: extra_info)
+                                  extra_info: extra_info.merge({log_severity: 'error'}))
   end
 
   def warn(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_warning(message, category: category, user_id: user_id, account_id: account_id,
-                                  extra_info: extra_info)
+                                  extra_info: extra_info.merge({log_severity: 'warn'}))
   end
 
   def info(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_info(message, category: category, user_id: user_id, account_id: account_id,
-                                  extra_info: extra_info)
+                                  extra_info: extra_info.merge({log_severity: 'info'}))
   end
 
   def debug(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_debug(message, category: category, user_id: user_id, account_id: account_id,
-                                  extra_info: extra_info)
+                                  extra_info: extra_info.merge({log_severity: 'debug'}))
   end
 end
--- lib/plogger/version.rb
+++ lib/plogger/version.rb
@@ -1,3 +1,3 @@
 module Plogger
-  VERSION = "0.9.1"
+  VERSION = "0.9.2"
 end
