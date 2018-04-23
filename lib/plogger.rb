9405bd9bf620a84063caec16cc9b1c27b8e0b81b
Daniel Merrill <danielmerrill6@gmail.com>
2018-04-23 11:05:35 -0300

    Fix default value for log type

--- lib/plogger.rb
+++ lib/plogger.rb
@@ -20,31 +20,31 @@ module Plogger
     @@config = config
   end
 
-  def exception(exception, type: '', category: '', user_id: '', account_id: '', extra_info: {})
+  def exception(exception, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_exception(exception, category: category, user_id: user_id, account_id: account_id,
                                         extra_info: extra_info)
   end
 
-  def error(message, type: '', category: '', user_id: '', account_id: '', extra_info: {})
+  def error(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_error(message, category: category, user_id: user_id, account_id: account_id,
                                   extra_info: extra_info)
   end
 
-  def warn(message, type: '', category: '', user_id: '', account_id: '', extra_info: {})
+  def warn(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_warning(message, category: category, user_id: user_id, account_id: account_id,
                                   extra_info: extra_info)
   end
 
-  def info(message, type: '', category: '', user_id: '', account_id: '', extra_info: {})
+  def info(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_info(message, category: category, user_id: user_id, account_id: account_id,
                                   extra_info: extra_info)
   end
 
-  def debug(message, type: '', category: '', user_id: '', account_id: '', extra_info: {})
+  def debug(message, type: 'system', category: '', user_id: '', account_id: '', extra_info: {})
     handler = Plogger::Handler.new(config)
     handler.handle_debug(message, category: category, user_id: user_id, account_id: account_id,
                                   extra_info: extra_info)
